b127184a7f315c6d89ed2eed5dced863fcb206d3
ShortException <shortexceptionde@gmail.com>
2023-02-22 20:07:12 +0100

    - changed map key type from int to string - added method to get queue instance using queue name

--- src/main/java/com/laudynetwork/networkutils/api/queue/QueueManager.java
+++ src/main/java/com/laudynetwork/networkutils/api/queue/QueueManager.java
@@ -1,22 +1,65 @@
 package com.laudynetwork.networkutils.api.queue;
 
 import com.laudynetwork.networkutils.api.sql.SQLConnection;
+import com.laudynetwork.networkutils.api.sql.SQLWrapper;
+import lombok.SneakyThrows;
 import lombok.val;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public record QueueManager(SQLConnection sqlConnection) {
 
-    private static final Map<Integer, Queue> QUEUE_MAP = new HashMap<>();
+    private static final Map<String, Queue> QUEUE_MAP = new HashMap<>();
 
     private void checkTable() {
         this.sqlConnection.createTableFromSQL("create table if not exist minecraft_networkutils_queues(queue varchar(30) not null, destServer varchar(30) not null, waitingPlayers text not null, maxSlots int not null, minSlots int not null, id int auto_increment primary key);");
+    }
+
+    @SneakyThrows
+    public Queue getQueueFromName(String queueName) {
+
+        if (!QUEUE_MAP.containsKey(queueName)) {
+
+            CompletableFuture<Queue> future = new CompletableFuture<>();
+
+            this.sqlConnection.resultSet("select * from minecraft_networkutils_queues where queue = '" + queueName + "'", resultSet -> {
+
+                try {
+                    while (resultSet.next()) {
+
+                        val destServer = resultSet.getString("destServer");
+                        val waitingPlayers = resultSet.getString("waitingPlayers");
+                        val maxSlots = resultSet.getInt("maxSlots");
+                        val minSlots = resultSet.getInt("minSlots");
+                        val id = resultSet.getInt("id");
+
+                        val queuePlayers = SQLWrapper.fromStringToList(waitingPlayers).stream()
+                                .map(uuidString -> new QueuePlayer(UUID.fromString(uuidString), this.sqlConnection)).toList();
+
+                        future.complete(new Queue(queueName, id, queuePlayers, destServer, null, maxSlots, minSlots));
+
+                    }
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+
+            return future.get();
+
+        } else
+            return QUEUE_MAP.get(queueName);
+
+
     }
 
     public Queue createQueue(String destinationServer, int maxSlots, int minSlots) {
@@ -41,7 +84,7 @@ public record QueueManager(SQLConnection sqlConnection) {
 
         val queue = new Queue(destinationServer, id, new ArrayList<>(), destinationServer, bossbar, maxSlots, minSlots);
 
-        QUEUE_MAP.put(id, queue);
+        QUEUE_MAP.put(queueName, queue);
 
         return queue;
     }
