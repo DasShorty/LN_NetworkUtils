1a3dc17ffd15963fc689cf5d06e82d030bd2a287
ShortException <shortexceptionde@gmail.com>
2023-03-19 19:51:09 +0100

    fix(language): added player selected language

--- src/main/java/com/laudynetwork/networkutils/listeners/CommandProtectionListener.java
+++ src/main/java/com/laudynetwork/networkutils/listeners/CommandProtectionListener.java
@@ -2,7 +2,7 @@ package com.laudynetwork.networkutils.listeners;
 
 import com.laudynetwork.networkutils.api.messanger.api.MessageAPI;
 import com.laudynetwork.networkutils.api.messanger.backend.MessageBackend;
-import com.laudynetwork.networkutils.api.messanger.backend.TranslationLanguage;
+import com.laudynetwork.networkutils.api.player.NetworkPlayer;
 import lombok.val;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Bukkit;
@@ -14,9 +14,11 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 public class CommandProtectionListener implements Listener {
 
     private final MessageAPI msgAPI;
+    private final MessageBackend msgBackend;
 
     public CommandProtectionListener(MessageBackend msgBackend) {
         this.msgAPI = new MessageAPI(msgBackend, MessageAPI.PrefixType.SYSTEM);
+        this.msgBackend = msgBackend;
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
@@ -26,24 +28,30 @@ public class CommandProtectionListener implements Listener {
 
         val command = message.split(" ")[0];
 
+        val player = event.getPlayer();
+        val networkPlayer = new NetworkPlayer(this.msgBackend.getConnection(), player.getUniqueId());
+
+        val language = networkPlayer.getLanguage();
+
         if (Bukkit.getHelpMap().getHelpTopic(command) == null) {
             event.setCancelled(true);
-            event.getPlayer().sendMessage(this.msgAPI.getMessage(TranslationLanguage.ENGLISH, "command.unknown", Placeholder.unparsed("command", command)));
+            player.sendMessage(this.msgAPI.getMessage(language, "command.unknown", Placeholder.unparsed("command", command)));
 
             return;
         }
 
-        if (event.getPlayer().isOp())
+        if (player.isOp())
             return;
+
 
         switch (command.split(":")[0].replace("/", "")) {
             case "help", "?", "bukkit", "paper", "spigot", "ver", "version", "info" -> {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(this.msgAPI.getMessage(TranslationLanguage.ENGLISH, "command.permission.missing"));
+                player.sendMessage(this.msgAPI.getMessage(language, "command.missing.permission"));
             }
             case "pl", "plugins" -> {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(this.msgAPI.getMessage(TranslationLanguage.ENGLISH, "command.plugins.show"));
+                player.sendMessage(this.msgAPI.getMessage(language, "command.plugins.show"));
             }
         }
 
