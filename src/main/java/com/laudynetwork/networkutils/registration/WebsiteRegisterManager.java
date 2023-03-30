eb8ddd32d58d3a2e252a78df5dd618fe8908394c
anthony.timmel <anthony.timmel@laudynetwork.com>
2023-03-30 15:22:05 +0200

    build(redis updated): redis updated to 4.5.0-FINAL and refactored code

--- src/main/java/com/laudynetwork/networkutils/registration/WebsiteRegisterManager.java
+++ src/main/java/com/laudynetwork/networkutils/registration/WebsiteRegisterManager.java
@@ -2,9 +2,12 @@ package com.laudynetwork.networkutils.registration;
 
 import com.laudynetwork.networkutils.api.redis.Redis;
 import com.laudynetwork.networkutils.api.sql.SQLConnection;
+import lombok.SneakyThrows;
+import lombok.val;
 
 import java.time.Duration;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.regex.Pattern;
 
 public class WebsiteRegisterManager {
@@ -17,18 +20,24 @@ public class WebsiteRegisterManager {
         this.redis = redis;
     }
 
-    public RegisteredUser addUser(UUID uuid, String email) {
-        if (this.sqlConnection.existsColumn("website_users", "uuid", uuid.toString()))
-            return new RegisteredUser(false, null);
+    @SneakyThrows
+    public CompletableFuture<RegisteredUser> addUser(UUID uuid, String email) {
+        val future = new CompletableFuture<RegisteredUser>();
+
+        if (this.sqlConnection.existsColumn("website_users", "uuid", uuid.toString())) {
+            future.complete(new RegisteredUser(false, null));
+            return future;
+        }
 
         UUID token = UUID.randomUUID();
 
         this.sqlConnection.insert("website_users", new SQLConnection.DataColumn("uuid", uuid.toString()),
                 new SQLConnection.DataColumn("email", email));
 
-        this.redis.getConnection().setex(token.toString(), Duration.ofHours(2).toSeconds(), email);
+        val done = this.redis.getConnection().async().setex(token.toString(), Duration.ofHours(2).toSeconds(), email).isDone();
 
-        return new RegisteredUser(true, token);
+        future.complete(new RegisteredUser(true, token));
+        return future;
     }
 
     public boolean isValid(String email) {
