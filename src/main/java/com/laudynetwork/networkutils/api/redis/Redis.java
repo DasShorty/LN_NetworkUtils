c931a9c90e55f100b380d8f4d8c6680de80ebfb2
anthony.timmel <anthony.timmel@laudynetwork.com>
2023-03-30 15:13:11 +0200

    build(redis updated): redis updated to 4.5.0-FINAL and refactored code

--- src/main/java/com/laudynetwork/networkutils/api/redis/Redis.java
+++ src/main/java/com/laudynetwork/networkutils/api/redis/Redis.java
@@ -3,17 +3,17 @@ package com.laudynetwork.networkutils.api.redis;
 import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisConnection;
 import com.lambdaworks.redis.RedisURI;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
 import lombok.Getter;
 
 @Getter
 public class Redis {
 
-    private final RedisConnection<String, String> connection;
+    private final StatefulRedisConnection<String, String> connection;
     private final RedisClient client;
 
     public Redis() {
-        this.client = new RedisClient(
-                RedisURI.create("redis://RlUD7l3AC5Lg9UyC3BTjxIsn6Lw984TG@89.163.129.221:6379"));
+        this.client = RedisClient.create("redis://RlUD7l3AC5Lg9UyC3BTjxIsn6Lw984TG@89.163.129.221:6379");
         this.connection = this.client.connect();
     }
 
