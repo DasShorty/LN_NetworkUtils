f287f772b0d75c6c7bc1e39edcc596a16e0928e8
anthony.timmel <anthony.timmel@laudynetwork.com>
2023-04-02 20:22:17 +0200

    Changes by ferri

--- src/main/java/com/laudynetwork/networkutils/api/player/NetworkPlayer.java
+++ src/main/java/com/laudynetwork/networkutils/api/player/NetworkPlayer.java
@@ -1,7 +1,9 @@
 package com.laudynetwork.networkutils.api.player;
 
+import com.laudynetwork.database.mysql.MySQL;
+import com.laudynetwork.database.mysql.utils.Select;
+import com.laudynetwork.database.mysql.utils.UpdateValue;
 import com.laudynetwork.networkutils.api.messanger.backend.TranslationLanguage;
-import com.laudynetwork.networkutils.api.sql.SQLConnection;
 import com.viaversion.viaversion.api.Via;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
@@ -12,18 +14,15 @@ import java.util.UUID;
 @SuppressWarnings("SqlResolve")
 public class NetworkPlayer {
 
-    private final SQLConnection connection;
+    private final MySQL sql;
     private final UUID uuid;
     private final Logger logger;
 
-    public NetworkPlayer(SQLConnection connection, UUID uuid) {
-        this.connection = connection;
+    public NetworkPlayer(MySQL sql, UUID uuid) {
+        this.sql = sql;
         this.uuid = uuid;
 
         this.logger = LoggerFactory.getLogger("NetworkPlayer");
-
-        connection.createTableWithPrimaryKey("minecraft_general_playerData", "uuid", new SQLConnection.TableColumn("uuid", SQLConnection.ColumnType.VARCHAR, 35),
-                new SQLConnection.TableColumn("language", SQLConnection.ColumnType.VARCHAR, 20));
     }
 
     public ProtocolVersion getPlayerVersion() {
@@ -33,7 +32,9 @@ public class NetworkPlayer {
     @SneakyThrows
     public TranslationLanguage getLanguage() {
 
-        if (!connection.existsColumn("minecraft_general_playerData", "uuid", uuid)) {
+        var select = new Select("minecraft_general_playerData", "*", "uuid = '" + uuid.toString() + "'");
+
+        if (!sql.rowExist(select)) {
             setLanguage(TranslationLanguage.ENGLISH);
             return TranslationLanguage.ENGLISH;
         }
@@ -41,22 +42,18 @@ public class NetworkPlayer {
         var result = sql.rowSelect(select);
         var language = (String) result.getRows().get(0).get("language");
 
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        return TranslationLanguage.getFromDBName(future.get().toUpperCase());
+        return TranslationLanguage.getFromDBName(language);
     }
 
     public void setLanguage(TranslationLanguage language) {
-        if (connection.existsColumn("minecraft_general_playerData", "uuid", uuid.toString())) {
+        var select = new Select("minecraft_general_playerData", "*", "uuid = '" + uuid.toString() + "'");
+
+        if (sql.rowExist(select)) {
             logger.info("Updating language for [" + uuid + "] to " + language.name());
-            connection.update("minecraft_general_playerData", "uuid", uuid.toString(), new SQLConnection.DataColumn("language", language.getDbName()));
+            sql.rowUpdate("minecraft_general_playerData", "uuid = '" + uuid + "'", new UpdateValue("language", language.getDbName()));
         } else {
             logger.info("Creating language for [" + uuid + "] with language " + language.name());
-            connection.insert("minecraft_general_playerData", new SQLConnection.DataColumn("uuid", uuid.toString()), new SQLConnection.DataColumn("language", language.getDbName()));
+            sql.tableInsert("minecraft_general_playerData", "uuid, language", uuid.toString(), language.getDbName());
         }
     }
 }
--- src/main/java/com/laudynetwork/networkutils/essentials/vanish/VanishedPlayer.java
+++ src/main/java/com/laudynetwork/networkutils/essentials/vanish/VanishedPlayer.java
@@ -1,7 +1,8 @@
 package com.laudynetwork.networkutils.essentials.vanish;
 
+import com.laudynetwork.database.mysql.utils.Select;
+import com.laudynetwork.database.mysql.utils.UpdateValue;
 import com.laudynetwork.networkutils.NetworkUtils;
-import com.laudynetwork.networkutils.api.sql.SQLConnection;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.bukkit.Bukkit;
@@ -10,14 +11,7 @@ import java.util.UUID;
 
 public record VanishedPlayer(UUID uuid) {
 
-    private void checkTable() {
-        NetworkUtils.getINSTANCE().getDbConnection().createTableWithPrimaryKey("minecraft_networktutils_essentials_vanish", "uuid", new SQLConnection.TableColumn("uuid", SQLConnection.ColumnType.VARCHAR, 35),
-                new SQLConnection.TableColumn("vanished", SQLConnection.ColumnType.INTEGER, 1));
-    }
-
     public VanishedPlayer update(boolean vanished) {
-
-        checkTable();
 
         val player = Bukkit.getPlayer(this.uuid);
         assert player != null;
@@ -38,46 +32,28 @@ public record VanishedPlayer(UUID uuid) {
     @SneakyThrows
     public boolean isVanished() {
 
-        checkTable();
+        val sql = NetworkUtils.getINSTANCE().getSql();
+        val select = new Select("minecraft_networktutils_essentials_vanish", "*", "uuid = '" + this.uuid.toString() + "'");
 
-        val sqlConnection = NetworkUtils.getINSTANCE().getDbConnection();
-
-        if (!sqlConnection.existsColumn("minecraft_networktutils_essentials_vanish", "uuid", this.uuid.toString())) {
+        if (!sql.rowExist(select)) {
             return false;
         }
 
-        CompletableFuture<Integer> future = new CompletableFuture<>();
-
-        sqlConnection.resultSet("SELECT * FROM minecraft_networktutils_essentials_vanish WHERE `uuid`='" + this.uuid + "'", resultSet -> {
-
-            try {
-
-                while (resultSet.next()) {
-
-                    future.complete(resultSet.getInt("vanished"));
-
-                }
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        return future.get() == 1;
-
+        return ((Integer) sql.rowSelect(select).getRows().get(0).get("vanished")) == 1;
     }
 
     private void updateDB(boolean vanished) {
 
-        checkTable();
+        val sql = NetworkUtils.getINSTANCE().getSql();
+        val select = new Select("minecraft_networktutils_essentials_vanish", "*", "uuid = '" + uuid.toString() + "'");
 
-        val sqlConnection = NetworkUtils.getINSTANCE().getDbConnection();
-        if (!sqlConnection.existsColumn("minecraft_networktutils_essentials_vanish", "uuid", this.uuid.toString())) {
-            sqlConnection.insert("minecraft_networktutils_essentials_vanish", new SQLConnection.DataColumn("uuid", this.uuid.toString()),
-                    new SQLConnection.DataColumn("vanished", vanished ? 1 : 0));
+
+        if (!sql.rowExist(select)) {
+            sql.tableInsert("minecraft_networktutils_essentials_vanish", "uuid, vanished", uuid.toString(), vanished ? 1 : 0);
+            return;
         }
-        sqlConnection.update("minecraft_networktutils_essentials_vanish", "uuid", this.uuid.toString(),
-                new SQLConnection.DataColumn("vanished", vanished ? 1 : 0));
+
+        sql.rowUpdate("minecraft_networktutils_essentials_vanish", "uuid = '" + uuid + "'", new UpdateValue("vanished", vanished ? 1 : 0));
     }
 
 }
