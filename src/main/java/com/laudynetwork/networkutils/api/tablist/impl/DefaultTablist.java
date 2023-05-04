2bd2969d3e271219d0d95454410c608c532c673d
anthony.timmel <anthony.timmel@laudynetwork.com>
2023-05-04 21:51:14 +0200

    refactor(NU-9): in tablist is the right color displayed but in the nametag it is the color from rank

--- src/main/java/com/laudynetwork/networkutils/api/tablist/impl/DefaultTablist.java
+++ src/main/java/com/laudynetwork/networkutils/api/tablist/impl/DefaultTablist.java
@@ -1,5 +1,6 @@
 package com.laudynetwork.networkutils.api.tablist.impl;
 
+import com.laudynetwork.networkutils.api.Pair;
 import com.laudynetwork.networkutils.api.tablist.Tablist;
 import com.laudynetwork.networkutils.api.tablist.TablistPlayer;
 import com.laudynetwork.networkutils.essentials.vanish.VanishedPlayer;
@@ -7,7 +8,6 @@ import lombok.val;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -30,8 +30,12 @@ public class DefaultTablist implements Tablist {
         VanishedPlayer vanishedPlayer = new VanishedPlayer(player.getUniqueId());
         val tablistPlayer = new TablistPlayer(player, this.luckPerms);
 
-        val vanishedTeam = getVanishedTeam(scoreboard, tablistPlayer);
-        val rankTeam = getTeamFromRank(scoreboard, tablistPlayer);
+
+        val vanishedTeamFromRank = getVanishedTeam(scoreboard, tablistPlayer);
+        val vanishedTeam = vanishedTeamFromRank.key();
+        val teamFromRank = getTeamFromRank(scoreboard, tablistPlayer, player.getName());
+        val rankTeam = teamFromRank.key();
+
 
         if (vanishedPlayer.isVanished()) {
 
@@ -42,6 +46,7 @@ public class DefaultTablist implements Tablist {
                 vanishedTeam.addPlayer(player);
             }
 
+            player.playerListName(vanishedTeamFromRank.value());
 
         } else {
 
@@ -51,39 +56,34 @@ public class DefaultTablist implements Tablist {
             if (!rankTeam.hasPlayer(player)) {
                 rankTeam.addPlayer(player);
             }
+
+            player.playerListName(teamFromRank.value());
         }
     }
 
-    private Team getVanishedTeam(Scoreboard scoreboard, TablistPlayer tablistPlayer) {
+    private Pair<Team, Component> getVanishedTeam(Scoreboard scoreboard, TablistPlayer tablistPlayer) {
         val vanishWeight = tablistPlayer.getVanishWeight(tablistPlayer.getGroupName());
         var vanishedTeam = scoreboard.getTeam(vanishWeight);
         if (vanishedTeam == null)
             vanishedTeam = scoreboard.registerNewTeam(vanishWeight);
 
-        vanishedTeam.prefix(
-                Component.text(
-                                Character.toString((char)
-                                        Integer.parseInt("e10b", 16)))
-                        .color(TextColor.color(0xFFFFFF)).append(Component.space())
-                        .append(tablistPlayer.getGroupPrefix(tablistPlayer.getGroupName()))
-        );
-
         vanishedTeam.color(NamedTextColor.GRAY);
 
-        return vanishedTeam;
+        return new Pair<>(vanishedTeam, Component.text(Character.toString((char)
+                        Integer.parseInt("e10b", 16)))
+                .color(TextColor.color(0xFFFFFF)).append(Component.space())
+                .append(tablistPlayer.getGroupPrefix(tablistPlayer.getGroupName())));
     }
 
-    private Team getTeamFromRank(Scoreboard scoreboard, TablistPlayer tablistPlayer) {
+    private Pair<Team, Component> getTeamFromRank(Scoreboard scoreboard, TablistPlayer tablistPlayer, String playerName) {
         val groupName = tablistPlayer.getGroupName();
         Team team = scoreboard.getTeam(tablistPlayer.getWeight(groupName) + groupName);
 
         if (team == null)
             team = scoreboard.registerNewTeam(tablistPlayer.getWeight(groupName) + groupName);
 
-
-        team.prefix(tablistPlayer.getGroupPrefix(groupName));
         team.color(NamedTextColor.nearestTo(tablistPlayer.getRankColor(groupName)));
-        return team;
+        return new Pair<>(team, tablistPlayer.getGroupPrefix(groupName).append(Component.text(playerName).color(tablistPlayer.getRankColor(groupName))));
     }
 
 }
